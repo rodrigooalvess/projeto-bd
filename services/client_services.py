b921ec74ad6cb030be8b875e3cb747fcc98808b7
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-11-05 15:03:02 -0300

    Update client_services.py
    criacao da funcao de alteracao de endereco do cliente

--- services/client_services.py
+++ services/client_services.py
@@ -1,16 +1,36 @@
 from config.database import conectar_banco
+from psycopg2.errors import UniqueViolation
 
-def cadastrar_cliente(nome, cpf, endereco = 'Sem Endereço'):
-    conn = conectar_banco()
+def cadastrar_cliente(nome: str, cpf: str, endereco: str = "Sem Endereço"):
     try:
-        cursor = conn.cursor()
-        query = 'INSERT INTO CLIENTES (nome_cliente, cpf_cliente, endereco) values (%s, %s, %s)'
-        cursor.execute(query, (nome, cpf, endereco))
-        conn.commit()
-        print('Cliente cadastrado com sucesso!')
+        con = conectar_banco()
+        cursor = con.cursor()
+
+        sql = "INSERT INTO CLIENTES (nome_cliente, cpf_cliente, endereco) values (%s, %s, %s)"
+        cursor.execute(sql, (nome, cpf, endereco))
+        con.commit()
+
+        print(f"Cliente {cpf} cadastrado com sucesso!")
+    except UniqueViolation:
+        print(f"Cliente {cpf} já cadastrado")
     except Exception as erro:
-        print(f'Erro ao cadastrar cliente: {erro}')
+        print(f"Erro ao cadastrar cliente: {erro}")
     finally:
         cursor.close()
-        conn.close()
-        
+        con.close()
+
+def atualizar_endereco(id_cliente: int, endereco: str):
+    try:
+        con = conectar_banco()
+        cursor = con.cursor()
+
+        sql = "UPDATE CLIENTES SET endereco = %s WHERE id_cliente = %s"
+        cursor.execute(sql, (endereco, id_cliente))
+        con.commit()
+
+        print(f"Endereço atualizado com sucesso!")
+    except Exception as erro:
+        print(f"Erro ao atualizar dados do cliente: {erro}")
+    finally:
+        cursor.close()
+        con.close()
