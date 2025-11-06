eeff737da180b0a2c913c585d54a2b65c91dcdab
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-11-06 00:34:35 -0300

    Update funcionarios_services.py

--- services/funcionarios_services.py
+++ services/funcionarios_services.py
@@ -1,24 +1,5 @@
-from psycopg2.errors import UniqueViolation
 from config.database import conectar_banco
 
-def cadastro_funcionario(nome: str, cpf: str, cargo: str, senha: str):
-    try:
-        con = conectar_banco()
-        cursor = con.cursor()
-
-        sql = "INSERT INTO FUNCIONARIOS (nome_funcionario, cpf_funcionario, senha, cargo) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (nome, cpf, senha, cargo))
-        con.commit()
-        print(f"Usuário {cpf} cadastrado com sucesso !")
-
-    except UniqueViolation:
-        print(f"Erro: CPF {cpf} já cadstrado")
-        con.rollback() #desfaz tudo que estava sendo feito na transação atual (commit) e retorna ao estado antes dela começar.
-    except Exception as erro:
-        print(f"Erro: {erro}")
-    finally:
-        cursor.close()
-        con.close()
 
 def login(cpf, senha):
     try:
@@ -36,22 +17,3 @@ def login(cpf, senha):
         cursor.close()
         con.close()
 
-def listar_funcionarios():
-    try:
-        con = conectar_banco()
-        cursor = con.cursor()
-
-        sql = "SELECT id_funcionario, nome_funcionario FROM FUNCIONARIOS"
-        cursor.execute(sql)
-        users = cursor.fetchall()
-        #[(id1, nome1), (id2,nome2), ...]
-        if users:
-            for id, nome in users:
-                print(f"{id} - {nome}")
-        elif not users:
-            print("Nenhum Funcionário Cadastrado")
-    except Exception as erro:
-        print(f"Erro: {erro}")
-    finally:
-        cursor.close()
-        con.close()
