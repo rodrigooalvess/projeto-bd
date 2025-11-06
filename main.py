dc07ab90be195696757fae0fa37df4f272869b34
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-11-05 21:29:26 -0300

    Update main.py
    criacao da opcao de login e registro

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 import os
+from services import cadastro_funcionario, login
+
+def main():
+    while True: 
+        try:
+            print("1 - LOGIN \n2 - CADASTRO")
+            opc = int(input("Digite uma Opção: "))
+
+            if opc == 1:
+                print("-----LOGIN-----")
+                usuario = input("Usuário (CPF): ")
+                senha = input("Senha: ")
+                logged = login(usuario, senha)
+                if logged:
+                    print(f"Seja Bem-Vindo {usuario}")
+            elif opc == 2:
+                print("-----CADASTRO-----")
+                nome = input("Nome: ")
+                cpf = input("CPF: ") 
+                if cpf.len != 11:
+                    while True:
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        cpf = print("CPF Inválido, Digite Novamente: ")
+                        if cpf.len == 11:
+                            break
+                cargo = input("Cargo: ").lower()
+                senha = input("Senha: ")
+                cadastro_funcionario(nome, cpf, cargo, senha)
+        except TypeError:
+            print("Opção Inválida, Digite um Número!")
+        except Exception as erro:
+            print(f"Erro: {erro}")
+
+if __name__ == "__main__":
+    main()
