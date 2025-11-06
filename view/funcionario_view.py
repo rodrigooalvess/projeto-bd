e9fef90a18cd626e47764335d68d92dc7247e7cf
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-11-06 00:00:45 -0300

    Update funcionario_view.py
    criacao do painel do caixa

--- view/funcionario_view.py
+++ view/funcionario_view.py
@@ -1,22 +1,33 @@
 import os
-from services import cadastrar_cliente, validar_cpf, atualizar_endereco
+from services import cadastrar_cliente, validar_cpf, atualizar_endereco, procurar_cliente
 
 def caixa_painel(funcionario):
-    print("1 - CADASTRAR CLIENTE \n2 - ALTERAR/ADICIONAR ENDERECO DO CLIENTE \n3 - INICIAR COMPRA \n4 - SAIR")
-    opc = int(input("Digite uma Opção: "))
+    while True:
+        try:
+            print("1 - CADASTRAR CLIENTE \n2 - ALTERAR/ADICIONAR ENDERECO DO CLIENTE \n3 - INICIAR COMPRA \n4 - SAIR")
+            opc = int(input("Digite uma Opção: "))
 
-    if opc == 1:
-        print("-----CADASTRO DE CLIENTE-----")
-        nome = input("Nome: ")
-        cpf = validar_cpf()
-        opcao = int(input("1 - SIM \n2 - NÃO \nDeseja cadastrar endereço? "))
-        if opcao == 1:
-            endereco = input("Endereço: ")
-            cadastrar_cliente(nome, cpf, endereco)
-        else:
-            cadastrar_cliente(nome, cpf)
-    elif opc == 2:
-        print("-----ENDEREÇO-----")
-        cpf = validar_cpf()
-        endereco = input("Endereço: ")
-        atualizar_endereco(cpf, endereco)
+            if opc == 1:
+                print("-----CADASTRO DE CLIENTE-----")
+                nome = input("Nome: ")
+                cpf = validar_cpf()
+                opcao = int(input("1 - SIM \n2 - NÃO \nDeseja cadastrar endereço? "))
+                if opcao == 1:
+                    endereco = input("Endereço: ")
+                    cadastrar_cliente(nome, cpf, endereco)
+                else:
+                    cadastrar_cliente(nome, cpf)
+            elif opc == 2:
+                print("-----ENDEREÇO-----")
+                cpf = validar_cpf()
+                id = procurar_cliente(cpf)
+                endereco = input("Endereço: ")
+                atualizar_endereco(id, endereco)
+            elif opc == 3:
+                pass
+            elif opc == 4:
+                break
+            else:
+                print("Digite uma Opção Válida")
+        except TypeError:
+            print("Opção Inválida, Digite um Número")
