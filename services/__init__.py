51c49edb31141258104270519c240f2d02414839
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-11-06 00:39:25 -0300

    Update __init__.py
    import listar funcionarios

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,3 @@
-from .funcionarios_services import cadastro_funcionario, login
+from .funcionarios_services import cadastro_funcionario, login, listar_funcionarios
 from .validador_cpf import validar_cpf
 from .client_services import cadastrar_cliente, atualizar_endereco, procurar_cliente
