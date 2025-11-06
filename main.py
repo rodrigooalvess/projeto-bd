8e87ac46dc55d30f0262d7f19eeba5c42d4991a5
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-11-05 23:36:36 -0300

    Update main.py
    tirando funcao do cadastro do main pra adicionar no painel do funcionario

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-from services import cadastro_funcionario, login, validar_cpf
+from services import cadastro_funcionario, login, validar_cpf, cadastrar_cliente
 
 def main():
     while True: 
         try:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("1 - LOGIN \n2 - CADASTRO \n3 - SAIR \n4 - CADASTRO DE CLIENTE \n6 - SAIR")
+            print("1 - LOGIN \n2 - CADASTRO \n3 - SAIR")
             opc = int(input("Digite uma Opção: "))
 
             if opc == 1:
@@ -22,15 +22,10 @@ def main():
                 cargo = input("Cargo: ").lower()
                 senha = input("Senha: ")
                 cadastro_funcionario(nome, cpf, cargo, senha)
-            elif opc == 4:
-                print("-----CADASTRO DE CLIENTE-----")
-
-            elif opc == 6:
+            elif opc == 3:
                 break
         except TypeError:
             print("Opção Inválida, Digite um Número!")
-        except Exception as erro:
-            print(f"Erro: {erro}")
 
 if __name__ == "__main__":
     main()
